ec70b498eead73c097297060e5f94bfab9d6f5aa
Khôi Lê <15520385@gm.uit.edu.vn>
2018-06-07 15:22:47 +0700

    Schedule

---
+++ lib/models/schedule_item.dart
@@ -0,0 +1,9 @@
+class ScheduleItemDTO{
+  String MaChiTietGiangDay;
+  String Thu;
+  String Tiet;
+  String TenMon;
+  String TenLop;
+
+  ScheduleItemDTO(this.MaChiTietGiangDay,this.Thu,this.Tiet,this.TenLop,this.TenMon);
+}
